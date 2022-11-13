4d615f4b0fe4741d0a2869f7473749225eeb66b6
George <71167099+letow@users.noreply.github.com>
2022-11-13 20:46:32 +0500

    fixed types & refreshing every minute

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,29 @@ import Page from "./Components/Page/Page";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { AppDispatch } from "./Redux/Store";
-import { fetchNews } from "./API/ServerAPI";
+import { AppDispatch, RootState } from "./Redux/Store";
+import { fetchNews, fetchOneItem } from "./API/ServerAPI";
 
 function App() {
-  const lastNewsIDs = useSelector((state: any) => state.toolkit.lastNewsIDs);
+  const lastNewsIDs = useSelector(
+    (state: RootState) => state.toolkit.lastNewsIDs
+  );
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchNews());
+  }, []);
+
+  useEffect(() => {
+    lastNewsIDs.forEach((item: number) => dispatch(fetchOneItem(item)));
+  }, [lastNewsIDs]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("first");
+      dispatch(fetchNews());
+    }, 60000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -22,7 +36,9 @@ function App() {
           <Route path="/" component={Main} exact />
 
           {lastNewsIDs.map((item: number) => {
-            return <Route key={item} path={`/page/${item}`} component={Page} />;
+            return (
+              <Route key={item} path={`/page/${item}`} component={Page} exact />
+            );
           })}
 
           <Redirect to="/" />
